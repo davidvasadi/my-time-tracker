09ad9e4e3567d578f447b9ffca0c3d98ccc3f671
Vasadi David <david.vasadi@icloud.com>
2025-01-24 02:19:44 +0100

    vue config updated

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/my-time-tracker/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/my-time-tracker/'
+  : '/'
 })
